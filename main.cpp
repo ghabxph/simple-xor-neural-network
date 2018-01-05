e6944e70a23a31d172d8eeba434d0b63de677490
ghabxph <ghabxph.official@gmail.com>
2018-01-05 12:50:07 +0800

    Added MIT License

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/**
+ * Copyright 2017 ghabxph(c)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 #include "declarations.h"
 
 using namespace std;
@@ -11,16 +32,23 @@ int main()
     int i;
 
     i = 0;
-    while (neuralNetworkXor.performXor(1,1) != 0 && i < 10000) {
+    while (neuralNetworkXor.performXor(1,1) != 0 && i < 5) {
         neuralNetworkXor.trainNetwork(1,1,0);
         cout << i << endl;
         i++;
     }
 
+    i = 0;
+    while (neuralNetworkXor.performXor(1,1) != 0 && i < 5) {
+        neuralNetworkXor.trainNetwork(0,0,0);
+        cout << i << endl;
+        i++;
+    }
+
     cout << endl << "The XOR of 1 and 1 is: " << neuralNetworkXor.performXor(1,1) << endl;
-    cout << endl << "The XOR of 0 and 0 is: " << neuralNetworkXor.performXor(0,0) << endl;
-    cout << endl << "The XOR of 1 and 0 is: " << neuralNetworkXor.performXor(1,0) << endl;
-    cout << endl << "The XOR of 0 and 1 is: " << neuralNetworkXor.performXor(0,1) << endl;
+//    cout << endl << "The XOR of 0 and 0 is: " << neuralNetworkXor.performXor(0,0) << endl;
+//    cout << endl << "The XOR of 1 and 0 is: " << neuralNetworkXor.performXor(1,0) << endl;
+//    cout << endl << "The XOR of 0 and 1 is: " << neuralNetworkXor.performXor(0,1) << endl;
 
 
 }
